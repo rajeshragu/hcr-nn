a1bac6af53a0ba7a87a5087877b80cc12a8f2fe0
Rajesh Ragunathan <rajesh.ragunathan@sonata-software.com>
2017-12-12 19:52:19 +0530

    Training network

--- hcrapp/services/train-network.service.js
+++ hcrapp/services/train-network.service.js
@@ -1,21 +1,38 @@
 var synaptic = require('synaptic');
 var Jimp = require("jimp");
+var fs = require('fs');
 
-var IMG_SIZE = 50;
+var IMG_SIZE = 15;
 var IMG_UPLOAD_PATH = 'public/uploads/';
 var IMG_SRC = '';
 var IMG_DEST = '';
 var IMG_INDEX = 0;
+var IMG_CHARARR_STAGE_1 = '';
+var IMG_CHARARR_STAGE_2 = [];
+var IMG_CHARARR_STAGE_3 = [];
+var NETWORK_ARRAY = [];
+var NETWORK_INPUT_LAYERS = IMG_SIZE * IMG_SIZE; // IMG_SIZE^2
+var NETWORK_HIDDEN_LAYERS = 5;
+var NETWORK_OUTPUT_LAYERS = 5;
+var NETWORK_ITERATION = 2000;
+var NETWORK_LEARNING_RATE = .3;
+var OUTPUT_DATA = {
+  'A': [0,0,0,0,0]
+};
+
 // Saving the context of this module inside the _the variable
 _this = this;
 exports.trainNetwork = async function(trainingData){
   try{
+    console.log('--TargetCharacter--', TargetCharacter);
+    console.log('--Output--', OUTPUT_DATA[trainingData.TargetCharacter]);
     if(trainingData.DataSet.length>0){
+      NETWORK_ARRAY = [];
       readFileAndInitiate(IMG_INDEX);
     }
     return 'Success';
-  }catch(e){      
-    // return a Error message describing the reason     
+  }catch(e){
+    // return a Error message describing the reason
     throw Error("Error while training the network in service.")
   }
 
@@ -29,10 +46,42 @@ exports.trainNetwork = async function(trainingData){
         console.error(err);
       });
     }
+    else{
+      // create the network
+      var Layer = synaptic.Layer;
+      var Network = synaptic.Network;
+      var inputLayer = new Layer(NETWORK_INPUT_LAYERS);
+      var hiddenLayer = new Layer(NETWORK_HIDDEN_LAYERS);
+      var outputLayer = new Layer(NETWORK_OUTPUT_LAYERS);
+      inputLayer.project(hiddenLayer);
+      hiddenLayer.project(outputLayer);
+      var myNetwork = new Network({
+          input: inputLayer,
+          hidden: [hiddenLayer],
+          output: outputLayer
+      });
+      // train the network
+      var learningRate = NETWORK_LEARNING_RATE;
+      for(var i=0; i<NETWORK_ARRAY.length; i++){
+        for (var j=0; j<NETWORK_ITERATION; j++){
+          myNetwork.activate(NETWORK_ARRAY[i]);
+          myNetwork.propagate(learningRate, OUTPUT_DATA[trainingData.TargetCharacter]);
+        }
+      }
+      var exported = myNetwork.toJSON();
+      /*fs.writeFile(IMG_UPLOAD_PATH+'myNetwork.json', JSON.stringify(exported), networkExportCB);*/
+    }
+  }
+
+  function networkExportCB(){
+    console.log('Network Exported!!');
   }
 
   function processBW(i) {
     var pixelLog = '';
+    IMG_CHARARR_STAGE_1 = '';
+    IMG_CHARARR_STAGE_2 = [];
+    IMG_CHARARR_STAGE_3 = [];
     Jimp.read(IMG_DEST).then(function (img) {
       let image = img.mirror(true, false).rotate(270);
       for(x=0; x<IMG_SIZE; x++){
@@ -44,11 +93,18 @@ exports.trainNetwork = async function(trainingData){
           let b = pixelValue.b;
           //console.log('[',r,',',g,',',b,']');
           if((r+g+b) >=450) pixelColor = 0;
-          pixelLog += pixelColor;            
+          pixelLog += pixelColor;
+          IMG_CHARARR_STAGE_1 += pixelColor;
         }
         pixelLog += '\n';
       }
       console.log(pixelLog);
+      IMG_CHARARR_STAGE_2 = IMG_CHARARR_STAGE_1.split('');
+      for(let result of IMG_CHARARR_STAGE_2){
+        IMG_CHARARR_STAGE_3.push(parseInt(result, 2));
+      }
+      //console.log(IMG_CHARARR_STAGE_3.length);
+      NETWORK_ARRAY.push(IMG_CHARARR_STAGE_3);
       readFileAndInitiate(++IMG_INDEX);
     }).catch(function (err) {
         console.error(err);
